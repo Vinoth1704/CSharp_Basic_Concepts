ca49967ba21b25c3901b1f556e1e025f0c50635a
Vinoth1704 <vinoth.1704j@gmail.com>
2022-09-22 14:37:45 +0530

    Updated Classes
    Added operator overloading and passing object to methods.

--- Classes.cs
+++ Classes.cs
@@ -90,6 +90,99 @@ namespace Learn
 
     }
 
+    class BankAccount
+    {
+        private string? name;
+        private double balance;
+
+        public BankAccount(string name, double balance)
+        {
+            this.name = name;
+            this.balance = balance;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name!;
+            }
+        }
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+        }
+
+        public static BankAccount operator +(BankAccount bank1, BankAccount bank2)
+        {
+            string newAccountName = bank1.name + bank2.name;
+            double newBalance = bank1.balance + bank2.balance;
+            BankAccount newAccount = new BankAccount(newAccountName, newBalance);
+            return newAccount;
+        }
+        public static BankAccount operator +(BankAccount bank1, double balance)
+        {
+            string newAccountName = bank1.name!;
+            double newBalance = bank1.balance + balance;
+            BankAccount newAccount = new BankAccount(newAccountName, newBalance);
+            return newAccount;
+        }
+        public static BankAccount operator -(BankAccount bank1, BankAccount bank2)
+        {
+            string newAccountName = bank1.name + bank2.name;
+            double newBalance = bank1.balance - bank2.balance;
+            BankAccount newAccount = new BankAccount(newAccountName, newBalance);
+            return newAccount;
+        }
+    }
+
+    class Person
+    {
+        private string? name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name!;
+            }
+        }
+
+        public int Age
+        {
+            set
+            {
+                age = value;
+            }
+            get
+            {
+                return age!;
+            }
+        }
+
+        public static void changeName(Person p)
+        {
+            p.name = "Steve";
+        }
+    }
+
+
+
+
     class Classes
     {
         public static void Class()
@@ -115,6 +208,22 @@ namespace Learn
             //Properties
             e1.Name = "Tom";
             Console.WriteLine(e1.Name);
+
+            //Operator overloading
+            BankAccount bank1 = new BankAccount("Frank", 100);
+            BankAccount bank2 = new BankAccount("Steve", 200);
+            BankAccount bank3 = bank1 + bank2;
+            Console.WriteLine("Bank 3" + bank3.Name + " " + bank3.Balance);
+            BankAccount bank5 = bank3 + 500;
+            Console.WriteLine("Bank 5" + bank5.Name + " " + bank5.Balance);
+            BankAccount bank4 = bank2 - bank1;
+            Console.WriteLine("Bank 4" + bank4.Name + " " + bank4.Balance);
+
+            //Passing object to methods
+            Person p1 = new Person("Frank", 25);
+            Console.WriteLine($"Before changing name = {p1.Name}");
+            Person.changeName(p1);
+            Console.WriteLine($"After changing name = {p1.Name}");
 
         }
     }
